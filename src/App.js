d4a3d59e73b787aaf8ea8a6c04b5385c78a8c0dc
Hannes Widrig <hwidrig@hwidrig.local>
2018-06-13 11:14:11 -0400

    styling and fonts added to repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from 'react'
 
-const App = () => (
-  <div>
-    <header>
-      <h1>Welcome to React</h1>
-    </header>
-    <p>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-)
+const App = () => <div />
 
 export default App
