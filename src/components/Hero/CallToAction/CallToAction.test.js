8bb6fda416d05ae3ff0b37ea90d299dad9a9e6fd
Hannes Widrig <hanneswidrig@gmail.com>
2018-06-14 09:18:54 -0400

    Reliable Jest Testing for other components

--- src/components/Hero/CallToAction/CallToAction.test.js
+++ src/components/Hero/CallToAction/CallToAction.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import CallToAction from './CallToAction'
 
 it(' successfully loads CallToAction button', () => {
 	const div = document.createElement('div')
-	ReactDOM.render(<CallToAction />, div)
+	ReactDOM.render(
+		<BrowserRouter>
+			<CallToAction />
+		</BrowserRouter>,
+		div
+	)
 	ReactDOM.unmountComponentAtNode(div)
 })
--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Main from './Main'
 
 it(' successfully loads Main', () => {
 	const div = document.createElement('div')
-	ReactDOM.render(<Main />, div)
+	ReactDOM.render(
+		<BrowserRouter>
+			<Main />
+		</BrowserRouter>,
+		div
+	)
 	ReactDOM.unmountComponentAtNode(div)
 })
