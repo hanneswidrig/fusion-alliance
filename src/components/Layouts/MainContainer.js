706f943e82f43c544e445834f59ab8cc3c8a2fcf
Hannes Widrig <hanneswidrig@gmail.com>
2018-06-14 16:29:20 -0400

    Improved styled component debugging and new MainContainer

---
+++ src/components/Layouts/MainContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import theme from '../../styles/theme'
+
+const MainContainer = styled.div`
+	display: flex;
+	max-width: ${theme.sizes.maxWidth};
+`
+
+export default MainContainer
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroBanner from '../Hero/HeroBanner/HeroBanner'
 import VideoBanner from '../Hero/VideoBanner/VideoBanner'
+import MainContainer from '../Layouts/MainContainer'
 
 const Main = () => (
 	<main>
 		<HeroBanner />
 		<VideoBanner />
+		<MainContainer />
 	</main>
 )
 
