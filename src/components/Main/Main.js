9112e5a59cc31556ef32142c1acffcc117e6ed88
Hannes Widrig <hanneswidrig@gmail.com>
2018-06-15 09:01:06 -0400

    Abstraction for main container

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBanner from '../Hero/HeroBanner/HeroBanner'
 import VideoBanner from '../Hero/VideoBanner/VideoBanner'
-import MainContainer from '../Layouts/MainContainer'
+import MainContainer from '../Layouts/Container/MainContainer/MainContainer'
 
 const Main = () => (
 	<main>
