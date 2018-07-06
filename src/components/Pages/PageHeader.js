edfea87dd033935a8c010e7ae76f3b60c1e19439
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-06 15:35:34 -0400

    fixed spacing

--- src/components/Pages/PageHeader.js
+++ src/components/Pages/PageHeader.js
@@ -4,6 +4,7 @@ const PageHeader = styled.h1`
 	font-size: 2rem;
 	font-weight: bold;
 	font-family: ${props => props.fonts.metaWeb};
+	color: ${props => props.colors.grey};
 	text-align: center;
 	height: 96px;
 `
