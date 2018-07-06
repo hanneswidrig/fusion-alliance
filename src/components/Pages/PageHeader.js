cb1629cf99a05a1ce91bd2a15e377778b89ba769
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-06 15:33:06 -0400

    Proper font

--- src/components/Pages/PageHeader.js
+++ src/components/Pages/PageHeader.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const PageHeader = styled.h1`
 	font-size: 2rem;
 	font-weight: bold;
+	font-family: ${props => props.fonts.metaWeb};
 	text-align: center;
 	height: 96px;
 `
