95b68daa7bd30d0c6c557f2d4f6bfc9b948cac72
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-06 20:04:45 -0400

    100% coverage for first page

--- src/components/Pages/Expertise/NestedExpertise.test.js
+++ src/components/Pages/Expertise/NestedExpertise.test.js
@@ -3,13 +3,47 @@ import ReactDOM from 'react-dom'
 import { MemoryRouter } from 'react-router-dom'
 import NestedExpertise from './NestedExpertise'
 
-it(' successfully loads NestedExpertise', () => {
+const match = {
+	url: '/expertise',
+}
+
+it(' successfully loads default route', () => {
 	const div = document.createElement('div')
-	const match = {
-		url: '/expertise',
-	}
 	ReactDOM.render(
 		<MemoryRouter initialEntries={['/expertise']}>
+			<NestedExpertise match={match} />
+		</MemoryRouter>,
+		div
+	)
+	ReactDOM.unmountComponentAtNode(div)
+})
+
+it(' successfully loads insights filter', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<MemoryRouter initialEntries={['/expertise/insights']}>
+			<NestedExpertise match={match} />
+		</MemoryRouter>,
+		div
+	)
+	ReactDOM.unmountComponentAtNode(div)
+})
+
+it(' successfully loads foundations filter', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<MemoryRouter initialEntries={['/expertise/foundations']}>
+			<NestedExpertise match={match} />
+		</MemoryRouter>,
+		div
+	)
+	ReactDOM.unmountComponentAtNode(div)
+})
+
+it(' successfully loads experiences filter', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<MemoryRouter initialEntries={['/expertise/experiences']}>
 			<NestedExpertise match={match} />
 		</MemoryRouter>,
 		div
