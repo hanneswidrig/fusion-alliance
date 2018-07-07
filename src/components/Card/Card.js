3ac86dd5da7f84e2ee7ec016148f2c5e75a16c5d
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-07 18:23:28 -0400

    Refactor: /Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,43 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
-import MdArrowForward from 'react-icons/lib/md/arrow-forward'
-import { Link } from 'react-router-dom'
+import {
+	CardWrapper,
+	Icon,
+	ImgWrapper,
+	CardSection,
+	CardHeader,
+	CardTitle,
+	CardBody,
+} from './CardStyles'
 
-const CardWrapper = styled(Link)`
-	display: flex;
-	width: 100%;
+const CardContainer = CardWrapper.extend`
 	flex-direction: row;
-	border-radius: 8px;
-	background-color: #fff;
-	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15);
-	transition: 200ms ease-in-out;
-	cursor: pointer;
 	margin: 4px;
-	text-decoration: none;
-	&:hover {
-		box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.3);
-	}
 `
 
-const Icon = styled(MdArrowForward)`
-	width: 48px;
-	height: 48px;
-	padding: 8px;
-	color: ${props => (props.iconcolor ? props.iconcolor : props.colors.grey)};
-`
-
-const CardImg = styled.div`
+const CardImg = ImgWrapper.extend`
 	display: none;
-	user-select: none;
 	height: 180px;
 	width: 180px;
-	flex: 1;
-	background-color: red;
-	background: url(${props => props.image});
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	border-top-left-radius: 8px;
 	border-bottom-left-radius: 8px;
 	@media screen and (min-width: ${props => props.responsive.large}) {
 		& {
@@ -46,42 +26,21 @@ const CardImg = styled.div`
 	}
 `
 
-const CardSection = styled.div`
-	display: flex;
-	color: ${props => props.colors.grey};
-	flex-direction: column;
-	flex: 2;
-`
-
-const CardHeader = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-`
-
-const CardTitle = styled.div`
-	color: ${props => props.colors.grey};
-	font-weight: 600;
-	margin: 16px 16px 0 16px;
+const Title = CardTitle.extend`
 	width: calc(100% - 48px);
 `
 
-const CardBody = styled.div`
-	margin: 12px 16px 16px 16px;
-	line-height: 1.25em;
-`
-
 const Card = props => (
-	<CardWrapper {...props} to="#">
+	<CardContainer {...props} to="#">
 		<CardImg {...props} />
-		<CardSection {...props}>
+		<CardSection {...props} flex="2">
 			<CardHeader {...props}>
-				<CardTitle {...props}>{props.title}</CardTitle>
+				<Title {...props}>{props.title}</Title>
 				<Icon {...props} iconcolor={props.iconcolor} />
 			</CardHeader>
 			<CardBody {...props}>{props.body}</CardBody>
 		</CardSection>
-	</CardWrapper>
+	</CardContainer>
 )
 
 export default Card
--- src/components/Card/CardStyles.js
+++ src/components/Card/CardStyles.js
@@ -1,1 +1,58 @@
-module.exports = {}
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import MdArrowForward from 'react-icons/lib/md/arrow-forward'
+
+export const CardWrapper = styled(Link)`
+	width: 100%;
+	display: flex;
+	cursor: pointer;
+	border-radius: 8px;
+	text-decoration: none;
+	background-color: #fbfbfb;
+	transition: 200ms ease-in-out;
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15);
+	&:hover {
+		box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.3);
+	}
+`
+
+export const Icon = styled(MdArrowForward)`
+	width: 48px;
+	height: 48px;
+	padding: 8px;
+	color: ${props => (props.iconcolor ? props.iconcolor : props.colors.grey)};
+`
+
+export const ImgWrapper = styled.div`
+	user-select: none;
+	background: url(${props => props.image});
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	border-top-left-radius: 8px;
+	flex: 1;
+`
+
+export const CardSection = styled.div`
+	display: flex;
+	color: ${props => props.colors.grey};
+	flex-direction: column;
+	flex: ${props => (props.flex ? props.flex : 1)};
+`
+
+export const CardHeader = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`
+
+export const CardTitle = styled.div`
+	color: ${props => props.colors.grey};
+	font-weight: 600;
+	margin: 16px 16px 0 16px;
+`
+
+export const CardBody = styled.div`
+	margin: 12px 16px 16px 16px;
+	line-height: 1.25em;
+`
