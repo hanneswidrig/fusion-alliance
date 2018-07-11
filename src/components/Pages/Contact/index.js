66a7387acd59f788a102900746e24a5eda058b09
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-11 14:34:39 -0400

    Minor removal

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import { PageContainer, PageHeader } from '../PageStyles'
+import { PageContainer } from '../PageStyles'
 import NestedContact from './NestedContact'
 
 const Contact = ({ match }) => {
