f67bfba0e75b42a5ba83469672fc86d0dc5061e6
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-11 16:13:27 -0400

    Contact Page

--- src/components/Pages/Contact/NestedContact.js
+++ src/components/Pages/Contact/NestedContact.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Route } from 'react-router-dom'
 import { Placeholder } from '../PageStyles'
+
+const ContactUs = () => <ContactForm netlify />
+
+const ContactForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
 
 const NestedContact = ({ match }) => {
 	return (
 		<React.Fragment>
-			<Route exact path={match.url} component={() => <div />} />
+			<Route exact path={match.url} component={ContactUs} />
 			<Placeholder />
 		</React.Fragment>
 	)
