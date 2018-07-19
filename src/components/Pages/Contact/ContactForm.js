2ac35f0d8d3fda6425cc20ce71177a98eedd4f61
Hannes Widrig <hanneswidrig@gmail.com>
2018-07-19 16:01:53 -0400

    name form

--- src/components/Pages/Contact/ContactForm.js
+++ src/components/Pages/Contact/ContactForm.js
@@ -3,10 +3,43 @@ import React from 'react'
 class ContactForm extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = { name: '' }
+
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
 	}
+
+	handleChange(e) {
+		this.setState({ name: e.target.value })
+	}
+
+	handleSubmit(e) {
+		e.preventDefault()
+		console.log(this.state)
+	}
+
+	isEnterKey(e) {
+		if (e.which === 13) {
+			e.preventDefault()
+		}
+	}
+
 	render() {
-		return <div />
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label htmlFor="name">
+					Name:
+					<input
+						name="name"
+						type="text"
+						value={this.state.name}
+						onChange={e => this.setState({ name: e.target.value })}
+						onKeyPress={this.isEnterKey}
+					/>
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		)
 	}
 }
 
