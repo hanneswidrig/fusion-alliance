54c0aafd03df7dc85d71163b605e674f7cf0da28
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-01-19 18:21:09 -0500

    Fixed menu icon and testing

--- src/components/Navigation/MobileMenuButton/MobileMenuButton.js
+++ src/components/Navigation/MobileMenuButton/MobileMenuButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
-import MdMenu from 'react-icons/lib/md/menu'
+import { MdMenu } from 'react-icons/md'
 import theme from '../../../styles/theme'
 
 const MobileMenuWrapper = styled(NavLink)`
